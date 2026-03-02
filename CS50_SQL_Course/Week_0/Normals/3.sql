9a38a274428dd3b961fd6b379c25bb6b24fb012d
Sina Gholami Fashkhami <s7015801@studenti.unige.it>
2026-03-02 12:00:27 +0100

    Add files via upload

---
+++ CS50_SQL_Course/Week_0/Normals/3.sql
@@ -0,0 +1,1 @@
+SELECT "0m","100m","200m" FROM "normals" WHERE "latitude" = 85.5 AND "longitude" = 140.5;
---
+++ CS50_SQL_Course/Week_0/Normals/6.sql
@@ -0,0 +1,1 @@
+SELECT "longitude","latitude","50m" FROM "normals" WHERE ("longitude" BETWEEN "55" AND "75") AND ("latitude" BETWEEN "0" AND "20");
