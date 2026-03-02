7c6fc09638b1a6b9342963aaea0bba78df7dd069
Sina Gholami Fashkhami <s7015801@studenti.unige.it>
2026-03-02 12:27:32 +0100

    Add files via upload

---
+++ CS50_SQL_Course/Week_5/In_a_Snap/3.sql
@@ -0,0 +1,11 @@
+SELECT "to_user_id"
+FROM "messages"
+WHERE "from_user_id" = (
+    SELECT "id"
+    FROM "users"
+    WHERE "username" = 'creativewisdom377'
+)
+GROUP BY "to_user_id"
+ORDER BY COUNT("messages"."id") DESC
+LIMIT 3;
+
---
+++ CS50_SQL_Course/Week_5/In_a_Snap/4.sql
@@ -0,0 +1,7 @@
+SELECT "username"
+FROM "users"
+JOIN "messages" ON "messages"."to_user_id" = "users"."id"
+GROUP BY "to_user_id"
+ORDER BY COUNT("messages"."to_user_id") DESC
+LIMIT 1;
+
